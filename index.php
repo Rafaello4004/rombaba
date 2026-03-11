4e5138e834802a4fd3aa455a7f6dec981cc8b3a8
fwyosss <deriugin1234@gmail.com>
2026-03-11 15:30:07 +0300

    first labubu commit главная страница

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>mew</title>
+<meta charset="UTF-8"> 
+</head>
+<body>
+<h1>Дерюгин А. Д. | 9ПО-32</h1>
+<?php
+$str = "mememystrrrrrr";
+$num = 19937;
+$bool = false;
+$empty = null;
+echo $str;
+?>
+<!-- <p>Значение $str:  <?php echo var_dump($str)?> \nЗначение $num: var_dump($num) \nЗначение $bool: var_dump($bool) \nЗначение $empty: var_dump($empty)</p> -->
+</body>
+</html>
