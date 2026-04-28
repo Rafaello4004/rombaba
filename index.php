94211dcd8b1fcf0330375ba09a74dddb307a58ba
fwyosss <deriugin1234@gmail.com>
2026-04-28 13:33:24 +0300

    Добавлена форма регистрации

--- index.php
+++ index.php
@@ -7,6 +7,42 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
+    <h1>Форма регистрации</h1>
 
+    <form action="action.php" method="post">
+        <p>
+            <label for="name">Имя пользователя:</label>
+            <input type="text" id="name" name="name" placeholder="Введите имя" required>
+        </p>
+        <p>
+            <label for="email">E-mail:</label>
+            <input type="email" id="email" name="email" placeholder="Введите e-mail" required>
+        </p>
+        <p>
+            <label for="password">Пароль:</label>
+            <input type="password" id="password" name="password" placeholder="Введите пароль" required>
+        </p>
+        <p>
+            <label for="password">Подтвердите пароль:</label>
+            <input type="password" id="password" name="password" placeholder="Подтвердите пароль" required>
+        </p>
+        <p>
+            <label>Пол:</label>
+            <select name="gender">
+                <option value="">-- Выберите пол --</option>
+                <option value="male">Мужской</option>
+                <option value="female">Женский</option>
+            </select>
+        </p>
+        <p>
+            <label>
+                <input type="checkbox" name="agreement" value="yes" required>
+                Я согласен с условиями
+            </label>
+        </p>
+        <p>
+            <input type="submit" value="Зарегистрироваться">
+        </p>
+    </form>
 </body>
 </html>
