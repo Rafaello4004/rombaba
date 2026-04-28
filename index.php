45a53f74fa4031aafc1a67a9883eee24dde2d183
fwyosss <deriugin1234@gmail.com>
2026-04-28 13:21:50 +0300

    Первоначальная структура

--- index.php
+++ index.php
@@ -2,15 +2,11 @@
 <html lang="ru">
 <head>
     <meta charset="UTF-8">
-    <title>Главная</title>
-    <link rel="icon" type="image/png" href="/assets/images/fav.png">
-    <link rel="stylesheet" href="/assets/css/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>parapa</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <?php include 'includes/header.php'; ?>
-    <main>
-        <h1>Добро пожаловать!</h1>
-    </main>
-    <?php include 'includes/footer.php'; ?>
+
 </body>
 </html>
